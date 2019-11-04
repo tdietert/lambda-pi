9d1ef85397c2ddfe5eb1c31ca1a88d8101751298
tdietert <tdietert@mail.usf.edu>
2019-11-03 20:04:59 -0500

    Normal-order evaluation working

--- src/LambdaPi.hs
+++ src/LambdaPi.hs
@@ -2,12 +2,13 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveAnyClass             #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE LambdaCase                 #-}
 
 module LambdaPi
 where
@@ -93,7 +94,6 @@ data Value
   | VPi (Bind VUniTele Value) -- ^ A type abstraction value
   | VVar VVar -- ^ A free variable, "neutral" term
   | VApp Value Value -- ^ A value application to another value, "neutral" term
-  -- [WIP] Natural Number values
   | VNat
   | VZero
   | VSucc Value
@@ -122,11 +122,21 @@ throwErrorTypecheckM err = do
 eval :: Syn -> Result Value
 eval = runFreshMT . evalSyn mempty
 
-evalPretty :: Context -> Syn -> Result Doc
-evalPretty ctx = runFreshMT . (ppr <=< evalSyn ctx)
+evalSynPretty :: Context -> Syn -> Result Doc
+evalSynPretty = evalPretty evalSyn
 
 evalChkPretty :: Context -> Chk -> Result Doc
-evalChkPretty ctx = runFreshMT . (ppr <=< evalChk ctx)
+evalChkPretty = evalPretty evalChk
+
+evalPretty
+  :: (Context -> a -> TypecheckM Value)
+  -> Context
+  -> a
+  -> Result Doc
+evalPretty runTypecheckM ctx a = do
+  runFreshMT $ do
+    v <- runTypecheckM ctx a
+    ppr =<< normalize ctx v
 
 -- | Evaluation of terms
 --
@@ -165,7 +175,6 @@ evalSyn ctx syn = traceM ("evalSyn: " ++ show syn) >> case syn of
 
   -- TODO eval bug here prollably (See notebook):
   NatElim m mz ms k -> do
-    traceM $ "evalSyn: NatElim: " ++ show syn
     vk <- evalChk ctx k
     case vk of
       VZero -> evalChk ctx mz
@@ -177,24 +186,23 @@ evalSyn ctx syn = traceM ("evalSyn: " ++ show syn) >> case syn of
         vmsl <- vapp ctx vms l
         res <- vapp ctx vmsl rec
         pure res
-      -- TODO "Neutral" term for VNatElim?
-      VVar x -> do
-        vm <- evalChk ctx m
-        vmz <- evalChk ctx mz
-        vms <- evalChk ctx ms
-        pure (VNatElim vm vmz vms (VVar x))
+      -- -- TODO "Neutral" term for VNatElim?
+      -- VVar x -> do
+      --   vm <- evalChk ctx m
+      --   vmz <- evalChk ctx mz
+      --   vms <- evalChk ctx ms
+      --   pure (VNatElim vm vmz vms (VVar x))
       _ -> throwErrorTypecheckM "evalSyn: NatElim"
 
 -- | Helper function for value application
 vapp :: HasCallStack => Context -> Value -> Value -> TypecheckM Value
 vapp ctx ve ve' = do
-  pve <- render <$> ppr ve
-  pve' <- render <$> ppr ve'
-  traceM ("vapp: " ++ pve ++ " " ++ pve')
-  case ve of
+  ne <- normalize ctx ve
+  ne' <- normalize ctx ve'
+  case ne of
     VLam binder -> do
       (x, body) <- unbind binder
-      pure (subst x ve' body)
+      normalize ctx (subst x ne' body)
     -- This case embodies a "free" function name in the context
     VVar x -> do
       case lookup x ctx of
@@ -204,16 +212,8 @@ vapp ctx ve ve' = do
           case mv of
             Nothing -> pure $ VApp (VVar x) ve'
             Just v -> vapp ctx v ve'
-    -- This is a hack, we should have better methods of normalization:
-    VNatElim vm vmz vms vk -> do
-      case vk of
-        VZero -> vapp ctx vmz ve'
-        VSucc vl -> do
-          vmsl <- vapp ctx vms vl
-          vmsl' <- vapp ctx vmsl (VNatElim vm vmz vms vl)
-          vapp ctx vmsl' ve'
-        _ -> throwErrorTypecheckM $ "invalid apply: VNatElim: " ++ show ve
-    _ -> throwErrorTypecheckM $ "invalid apply: " ++ show ve
+    _ -> throwErrorTypecheckM $ "invalid apply: " ++ show ne
+
 
 -- | Evaluate a checkable expression
 evalChk :: HasCallStack => Context -> Chk -> TypecheckM Value
@@ -243,7 +243,7 @@ typecheckPretty' ctx = runFreshMT . (ppr <=< typeSyn ctx)
 
 -- | Compute the type of a term whose type can be synthesized given a context
 typeSyn :: HasCallStack => Context -> Syn -> TypecheckM Type
-typeSyn ctx syn = traceM ("typeSyn: " ++ show syn) >> case syn of
+typeSyn ctx = \case
   Var x -> case lookup (coerce x) ctx of
     Nothing -> throwErrorTypecheckM ("typeSyn: unknown identifier: " ++ show x)
     Just (VarInfo v t) -> pure t
@@ -253,39 +253,24 @@ typeSyn ctx syn = traceM ("typeSyn: " ++ show syn) >> case syn of
     typeChk ctx e t
     pure t
   App e e' -> do
-
-    pe <- render <$> ppr e
-    pe' <- render <$> ppr e'
-    traceM $ "typeSyn: " ++ pe ++ " applied to " ++ pe'
-
-    sigma <- do
-      sigma' <- typeSyn ctx e
-      case sigma' of
-        VApp ve ve' -> vapp ctx ve ve'
-        _ -> pure sigma'
+    sigma <- typeSyn ctx e
     case sigma of
       VPi binding -> do
-        psigma <- render <$> ppr sigma
-        traceM $ "typeSyn: Sigma: " ++ psigma
         (xt, t') <- unbind binding
         let ((x, Embed t), ()) = unrebind xt
-        px <- ppShow x
-        pt <- ppShow t
-        traceM $ "typeSyn: 'unrebind xt': " ++ px ++ " has type " ++ pt
         typeChk ctx e' t
         ve' <- evalChk ctx e'
-        pure (subst (coerce x) ve' t')
+        normalize ctx (subst (coerce x) ve' t')
       _ -> throwErrorTypecheckM ("illegal application: " ++ show sigma)
   Star    -> pure VStar
   Pi xp_p' -> do
     (xp, p') <- unbind xp_p'
-    let ((x,Embed p), ()) = unrebind xp
+    let ((x, Embed p), ()) = unrebind xp
     typeChk ctx p VStar
     t <- evalChk ctx p
     let varInfo = VarInfo Nothing t
     typeChk (insert (coerce x) varInfo ctx) p' VStar
     pure VStar
-  -- [WIP] Type "synthesis" of Natural number terms
   Nat -> pure VStar
   Zero -> pure VNat
   Succ k -> do
@@ -294,33 +279,31 @@ typeSyn ctx syn = traceM ("typeSyn: " ++ show syn) >> case syn of
   NatElim m mz ms k -> do
     typeChk ctx m (tarr VNat VStar)
     vm <- evalChk ctx m
-    traceM $ "Typechecking: " ++ (show mz)
-    traceM $ "as the type: " ++ (show vm)
-    traceM $ "but was before: " ++ (show m)
     typeChk ctx mz =<< vapp ctx vm VZero
     vk <- evalChk ctx k
     vmk <- vapp ctx vm vk
-    typeChk ctx ms =<<
+    t' <-
       pure . VPi . bind (mkVUniTele "l" VNat) . tarr vmk =<<
         vapp ctx vm (VSucc (VVar (s2n "l")))
+    typeChk ctx ms t'
     typeChk ctx k VNat
     pure vmk
 
+-- | A function type whose return type doesn't depend on the argument value
 tarr :: Type -> Type -> Type
 tarr a b = VPi (bind (mkVUniTele "_" a) b)
 
 -- | Check the type of a given type-checkable term
 typeChk :: HasCallStack => Context -> Chk -> Type -> TypecheckM ()
-typeChk ctx chk v = traceM ("typeChk: " ++ show chk) >> case chk of
+typeChk ctx chk v = case chk of
   Inf e -> do
     v' <- typeSyn ctx e
+    -- If not alpha equivalent, fail
     unless (aeq v v') $
       throwErrorTypecheckM $ unlines
-        [ "type mismatch syn:"
+        [ "type mismatch:"
         , "  expected: "++ show v
         , "  but got: " ++ show v'
-        , "  ENV: "
-        , show ctx
         ]
 
   Lam x_e -> case v of
@@ -330,23 +313,67 @@ typeChk ctx chk v = traceM ("typeChk: " ++ show chk) >> case chk of
       let ((_,Embed t),_) = unrebind xt
           varInfo = VarInfo Nothing t
       typeChk (insert (coerce x) varInfo ctx) e t'
-    -- A substitution for 've' may have occurred such that it's not in it's
-    -- normal form yet, and before type checking this lambda, we must force its
-    -- typexv <- .
-    VApp ve ve' -> typeChk ctx chk =<< vapp ctx ve ve'
     _ -> throwErrorTypecheckM $ unlines
         [ "type mismatch lam: "
         , "  expected VPi, but got: "++ show v
-        , "  ENV: "
-        , show ctx
         ]
 
 ----------------------------------------
 -- Normalize
 ----------------------------------------
 
--- Anytime a variable that has a value is looked up, we need to normalize its
--- substitution; or, every time a substitution is made, we need to normalize.
+-- | Normalize a value constructed by a substitution
+--
+-- The need for this function stems from the fact that the evalution strategy
+-- implicitly chosen during this dependent type theory study:
+--
+-- When function application is evaluated, or when typechecking the App
+-- expression, a variable substitution is performed to either compute the result
+-- of the function application, or compute the return type of a value. In both
+-- cases, a value is subsituted in place of a variable in the body of either a
+-- lambda or a pi term; The resulting body needs to be normalized, as a VApp
+-- value may contain a non-neutral term on the LHS that can be further
+-- normalized. Un-normalized terms cause the evalutor to choke and throw errors,
+-- as it always expects normalized terms. By calling 'normalize' on a resulting
+-- expression after a substitution is performed should prevent these errors,
+-- but commit us to a normal-order evaluation strategy.
+--
+normalize :: Context -> Value -> TypecheckM Value
+normalize ctx v =
+  case v of
+    VVar x -> pure (VVar x)
+    VLam binder -> do
+      (x, body) <- unbind binder
+      pure . VLam . bind x =<< normalize ctx body
+    VStar -> pure VStar
+    VPi binder -> do
+      (xt, t') <- unbind binder
+      let ((x, Embed t), ()) = unrebind xt
+      nt <- normalize ctx t
+      let nxt = mkVUniTele (name2String x) nt
+      nt' <- normalize ctx t'
+      pure $ VPi (bind nxt nt')
+    VApp ve ve' -> do
+      nve <- normalize ctx ve
+      nve' <- normalize ctx ve'
+      vapp ctx nve nve'
+    VNat -> pure VNat
+    VZero -> pure VZero
+    VSucc v -> pure . VSucc =<< normalize ctx v
+    VNatElim m mz ms k -> do
+      nk <- normalize ctx k
+      case nk of
+        VZero -> normalize ctx mz
+        VSucc l -> do
+          nm <- normalize ctx m
+          nmz <- normalize ctx mz
+          nms <- normalize ctx ms
+          nl <- normalize ctx l
+          let rec = VNatElim nm nmz nms nl
+          nmsl <- vapp ctx nms l
+          vapp ctx nmsl rec
+        VVar x -> pure $ VNatElim m mz ms nk
+        _ -> throwErrorTypecheckM "normalize: VNatElim"
 
 ----------------------------------------
 -- DSL for term easier construction
@@ -384,26 +411,7 @@ zero = Inf Zero
 succ' :: Syn -> Chk
 succ' = Inf . Succ . Inf
 
--- The reason this didn't work on its own:
---
---   N.B.
---     The "paper" code implements "natElim" in terms of a value that already
---     exists in the Context. If you define 'natElim' like this, it is not yet a
---     value, but since "Lam" is a "Chk" value, we cannot typecheck the Chk value,
---     without first giving it a type by annotating it. However, in order to
---     actually "apply" the lambda value 'natElim' to and values at all, we must
---     have a 'Syn' value as the function being applied. Thus, we need a _value_
---     'natElim' in the Context to construct a function "plus" that applies
---     "natElim" to arguments (as Var "natElim" is a 'Syn' value and not a 'Chk'
---     value).
 natElim =
-  lam "x" $
-    lam "y" $
-      lam "z" $
-        lam "a" $
-          Inf (NatElim (var "x") (var "y") (var "z") (var "a"))
-
-natElim' =
   vlam "x" $
     vlam "y" $
       vlam "z" $
@@ -418,12 +426,6 @@ vnatElim =
       varr (vpi "l" VNat (varr (VApp (vvar "m") (vvar "l")) (VApp (vvar "m") (VSucc (vvar "l"))))) $
         vpi "k" VNat (VApp (vvar "m") (vvar "k"))
 
-vnatElim' =
-  pi' "m" (arr (Inf Nat) (Inf Star)) $
-    arr (Inf (App (svar "m") (Inf Zero))) $
-      arr (pi' "l" (Inf Nat) (arr (Inf (App (svar "m") (var "l"))) (Inf (App (svar "m") (Inf (Succ (var "l"))))))) $
-        pi' "k" (Inf Nat) (Inf (App (svar "m") (var "k")))
-
 arr = pi' "_"
 varr = vpi "_"
 
@@ -434,7 +436,7 @@ varr = vpi "_"
 --   There are two contexts because in one
 stdlib :: Context
 stdlib = fromList
-  [ (s2n "natElim", VarInfo { varValue = Just natElim', varType = vnatElim }) ]
+  [ (s2n "natElim", VarInfo { varValue = Just natElim, varType = vnatElim }) ]
 
 stdlib_plus =
   App
@@ -446,10 +448,6 @@ stdlib_plus =
       (lam "n" (var "n"))
     )
     (lam "k" $ lam "rec" $ lam "n" $ Inf (Succ (Inf (App (svar "rec") (var "n")))))
-
-stdlib_plus_test =
-  App (Var $ s2n "natElim") $
-    lam "_" $ arr (Inf Nat) (Inf Nat)
 
 ----------------------------------------
 -- Pretty Printer
